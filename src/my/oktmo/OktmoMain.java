01718937972b40c254f97dd8ff5fbaa294a1b5d0
aanastasyaa <44464045+aanastasyaa@users.noreply.github.com>
2019-01-22 15:35:31 +0500

    Update OktmoMain.java

--- src/my/oktmo/OktmoMain.java
+++ src/my/oktmo/OktmoMain.java
@@ -8,6 +8,7 @@ public class OktmoMain {
      OktmoData data=new OktmoData();
      OktmoReader reader=new OktmoReader();
      reader.readPlacesAndGroups("data-201710.csv",data);
+  
      System.out.println(data.getSortedPlaces().toString());
      System.out.println(data.getAllStatuses().toString());
      System.out.println(data.getCommonMap().toString());
